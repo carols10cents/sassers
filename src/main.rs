fe83d66a5d83df6b9c3a54f8d1590c173027d30f
Carol Nichols <carol.nichols@gmail.com>
2015-05-24 20:40:57 -0400

    Binary with -v! It's aliiiive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate docopt;
+
+use docopt::Docopt;
+
 fn main() {
-    println!("Hello, world!");
+	const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+	static USAGE: &'static str = "
+	Usage:
+	  sassers [options]
+
+	Options:
+	    -h, --help         Show this message.
+	    -v, --version      Show the version of sassers.
+	";
+
+	let args = Docopt::new(USAGE)
+	                  .and_then(|d| d.parse())
+	                  .unwrap_or_else(|e| e.exit());
+
+	if args.get_bool("-v") {
+		println!("{}", VERSION);
+	}
 }
