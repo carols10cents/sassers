80b4e5931c7f2ed946f2be067601949046bf4a0d
Carol Nichols <carol.nichols@gmail.com>
2015-05-24 21:27:08 -0400

    Take style & input filename; output the input file contents.
    4392 f / 4499 tests

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
 extern crate docopt;
 
 use docopt::Docopt;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
-	const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-	static USAGE: &'static str = "
-	Usage:
-	  sassers [options]
+    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    static USAGE: &'static str = "
+Usage:
+    sassers [-t <style>] <inputfile>
+    sassers [-vh]
 
-	Options:
-	    -h, --help         Show this message.
-	    -v, --version      Show the version of sassers.
-	";
+Options:
+    -h, --help                   Show this message.
+    -v, --version                Show the version of sassers.
+    -t <style>, --style <style>  Output style.
+    ";
 
-	let args = Docopt::new(USAGE)
-	                  .and_then(|d| d.parse())
-	                  .unwrap_or_else(|e| e.exit());
+    let args = Docopt::new(USAGE)
+                      .and_then(|d| d.parse())
+                      .unwrap_or_else(|e| e.exit());
 
-	if args.get_bool("-v") {
-		println!("{}", VERSION);
-	}
+    if args.get_bool("-v") {
+        println!("{}", VERSION);
+    } else {
+        let style = args.get_str("-t");
+        let inputfile = args.get_str("<inputfile>");
+
+        let mut sass = String::new();
+        File::open(&Path::new(&inputfile)).unwrap().read_to_string(&mut sass).unwrap();
+
+        println!("{}", sass);
+    }
 }
