05d263bcba35b7f229462dc519cedaa67b387a86
Carol Nichols <carol.nichols@gmail.com>
2015-05-25 15:56:46 -0400

    Implement reading one character (bump)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 pub fn compile(sass: String, style: &str) -> String {
-    let parsed = SassReader::new(sass).parse();
+    let sr = SassReader::new(sass);
+    println!("{:?}", sr);
+    let parsed = sr.parse();
     match style {
         "nested"     => nested(parsed),
         "compressed" => compressed(parsed),
@@ -25,6 +27,7 @@ fn compact(sass: String) -> String {
     sass
 }
 
+#[derive(Debug)]
 struct SassReader {
     pub pos: u32,
     pub last_pos: u32,
@@ -34,15 +37,34 @@ struct SassReader {
 
 impl SassReader {
     pub fn new(str: String) -> SassReader {
-        SassReader {
+        let mut sr = SassReader {
             pos: 0,
             last_pos: 0,
             curr: Some('\n'),
             sass: str,
+        };
+        sr.bump();
+        sr
+    }
+
+    pub fn bump(&mut self) {
+        self.last_pos = self.pos;
+        let current_pos = self.pos as usize;
+        if current_pos < self.sass.len() {
+            let ch = char_at(&self.sass, current_pos);
+
+            self.pos = self.pos + (ch.len_utf8() as u32);
+            self.curr = Some(ch);
+        } else {
+            self.curr = None;
         }
     }
 
     pub fn parse(self) -> String {
         self.sass
     }
+}
+
+pub fn char_at(s: &str, byte: usize) -> char {
+    s[byte..].chars().next().unwrap()
 }
