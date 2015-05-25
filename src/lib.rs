9ddfbd6f446efe9eabc6d02d505e57bcb7efd251
Carol Nichols <carol.nichols@gmail.com>
2015-05-25 15:32:24 -0400

    Start of a parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub fn compile(sass: String, style: &str) -> String {
+    let parsed = SassReader::new(sass).parse();
     match style {
-        "nested"     => nested(sass),
-        "compressed" => compressed(sass),
-        "expanded"   => expanded(sass),
-        "compact"    => compact(sass),
+        "nested"     => nested(parsed),
+        "compressed" => compressed(parsed),
+        "expanded"   => expanded(parsed),
+        "compact"    => compact(parsed),
         _            => panic!("Unknown style: {}. Please specify one of nested, compressed, expanded, or compact.", style),
     }
 }
@@ -22,4 +23,26 @@ fn expanded(sass: String) -> String {
 
 fn compact(sass: String) -> String {
     sass
+}
+
+struct SassReader {
+    pub pos: u32,
+    pub last_pos: u32,
+    pub curr: Option<char>,
+    sass: String,
+}
+
+impl SassReader {
+    pub fn new(str: String) -> SassReader {
+        SassReader {
+            pos: 0,
+            last_pos: 0,
+            curr: Some('\n'),
+            sass: str,
+        }
+    }
+
+    pub fn parse(self) -> String {
+        self.sass
+    }
 }
