60a5fcc57d5800f9719b01624ed8866a781fcb25
Carol Nichols <carol.nichols@gmail.com>
2015-06-06 23:42:14 -0400

    And now I'm back to where I was a few days ago!!!!

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub fn compile(sass: &str, style: &str) -> Result<String, &'static str> {
         "nested"     => Ok(nested_output(&mut st)),
         "compressed" => Ok(compressed_output(&mut st)),
         "expanded"   => Ok(expanded_output(&mut st)),
-    //     "compact"    => Ok(parsed.compact(&sp)),
+        "compact"    => Ok(compact_output(&mut st)),
         _            => Err("Unknown style:. Please specify one of nested, compressed, expanded, or compact."),
     }
 }
@@ -56,6 +56,20 @@ pub fn expanded_output(tokenizer: &mut SassTokenizer) -> String {
             Event::Selector(name) => format!("{} {{", name),
             Event::Property(name, value) => format!("\n  {}: {};", name, value),
             Event::End(_) => format!("\n}}"),
+        };
+        output.push_str(print_token.as_str());
+    }
+    output
+}
+
+pub fn compact_output(tokenizer: &mut SassTokenizer) -> String {
+    let mut output =  String::from_str("");
+    while let Some(token) = tokenizer.next() {
+        let print_token = match token {
+            Event::Start(_) => continue,
+            Event::Selector(name) => format!("{} {{", name),
+            Event::Property(name, value) => format!(" {}: {};", name, value),
+            Event::End(_) => format!(" }}"),
         };
         output.push_str(print_token.as_str());
     }
