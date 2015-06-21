342d7593bb2dabe84cb635e6bd74ad53c1b414d2
Carol Nichols <carol.nichols@gmail.com>
2015-06-20 20:13:56 -0400

    Start of trying to make variablemapper work again

--- src/variable_mapper.rs
+++ src/variable_mapper.rs
@@ -1,4 +1,4 @@
-use event::Event;
+use event::{TopLevelEvent, Event, SassRule};
 use std::collections::HashMap;
 
 pub struct VariableMapper<I> {
@@ -25,21 +25,21 @@ impl<I> VariableMapper<I> {
 }
 
 impl<'a, I> Iterator for VariableMapper<I>
-    where I: Iterator<Item = Event<'a>>
+    where I: Iterator<Item = TopLevelEvent<'a>>
 {
-    type Item = Event<'a>;
+    type Item = TopLevelEvent<'a>;
 
-    fn next(&mut self) -> Option<Event<'a>> {
+    fn next(&mut self) -> Option<TopLevelEvent<'a>> {
         match self.tokenizer.next() {
-            Some(Event::Variable(name, value)) => {
+            Some(TopLevelEvent::SassVariable { name, value }) => {
                 let val = self.substitute_variables(&value);
                 self.variables.insert((*name).to_string(), val);
                 self.next()
             },
-            Some(Event::Property(name, value)) => {
-                let real_value = self.substitute_variables(&value);
-                Some(Event::Property(name, real_value.into()))
-            },
+            // Some(Event::Property(name, value)) => {
+            //     let real_value = self.substitute_variables(&value);
+            //     Some(Event::Property(name, real_value.into()))
+            // },
             other => other,
         }
     }
