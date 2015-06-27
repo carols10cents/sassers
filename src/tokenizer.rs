9445c05c450d5f6acad959877baf019f4e8b985c
Carol Nichols <carol.nichols@gmail.com>
2015-06-27 12:12:41 -0400

    Stop cloning self.state; use if/else instead of match
    To avoid borrowing. I don't really like this and there's probably a
    better way to do it with match but idk how rn...

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -71,71 +71,61 @@ impl<'a> Tokenizer<'a> {
 
         self.pick_something();
         while self.state != State::OutsideRules {
-            let curr_state = self.state.clone();
-            match curr_state {
-                State::Eof => {
-                    return None
-                },
-                State::InSelectors => {
-                    let sel = self.next_selector();
-                    if sel.is_some() {
-                        self.current_sass_rule.selectors.push(sel.unwrap());
-                    }
-                },
-                State::InProperties => {
-                    let prop = self.next_property();
-                    if prop.is_some() {
-                        self.current_sass_rule.children.push(prop.unwrap());
-                    }
-                },
-                State::EndRule => {
-                    // consume '}', should probably be checking that's what we actually have
-                    self.offset += 1;
+            if self.state == State::Eof { return None }
 
-                    match self.sass_rule_stack.pop() {
-                        Some(ref mut rule) => {
-                            rule.children.push(Event::ChildRule(self.current_sass_rule.clone()));
-                            self.current_sass_rule = (*rule).clone();
-                            self.pick_something();
-                        },
-                        None => self.state = State::OutsideRules,
-                    }
-                },
-                State::InRule => {
-                    self.sass_rule_stack.push(self.current_sass_rule.clone());
-                    self.current_sass_rule = SassRule::new();
-                    self.pick_something();
-                },
-                State::InVariable => {
-                    let var = self.next_variable();
-                    if var.is_some() {
-                        return var
-                    } else {
-                        // is this really what we should be doing here? reachable?
-                        self.state = State::Eof;
-                    }
-                },
-                State::InComment => {
-                    let comment = self.next_comment();
-                    if comment.is_some() {
-                        if self.sass_rule_stack.len() == 0 &&
-                           self.current_sass_rule.selectors.len() == 0 &&
-                           self.current_sass_rule.children.len() == 0 {
-                               return Some(TopLevelEvent::Comment(SassComment { comment: comment.unwrap() }))
-                        } else {
-                            self.current_sass_rule.children.push(comment.unwrap());
-                            self.pick_something();
-                        }
-                    } else {
-                        // is this really what we should be doing here? reachable?
-                        self.state = State::Eof;
-                    }
+            if self.state == State::InSelectors {
+                let sel = self.next_selector();
+                if sel.is_some() {
+                    self.current_sass_rule.selectors.push(sel.unwrap());
                 }
-                unknown_state => {
-                    println!("i dont know what to do for {:?}", unknown_state);
-                    println!("current sass rule = {:?}", self.current_sass_rule);
+            } else if self.state == State::InProperties {
+                let prop = self.next_property();
+                if prop.is_some() {
+                    self.current_sass_rule.children.push(prop.unwrap());
+                }
+            } else if self.state == State::EndRule {
+                // consume '}', should probably be checking that's what we actually have
+                self.offset += 1;
+
+                match self.sass_rule_stack.pop() {
+                    Some(ref mut rule) => {
+                        rule.children.push(Event::ChildRule(self.current_sass_rule.clone()));
+                        self.current_sass_rule = (*rule).clone();
+                        self.pick_something();
+                    },
+                    None => self.state = State::OutsideRules,
+                }
+            } else if self.state == State::InRule {
+                self.sass_rule_stack.push(self.current_sass_rule.clone());
+                self.current_sass_rule = SassRule::new();
+                self.pick_something();
+            } else if self.state == State::InVariable {
+                let var = self.next_variable();
+                if var.is_some() {
+                    return var
+                } else {
+                    // is this really what we should be doing here? reachable?
                     self.state = State::Eof;
-                },
+                }
+            } else if self.state == State::InComment {
+                let comment = self.next_comment();
+                if comment.is_some() {
+                    if self.sass_rule_stack.len() == 0 &&
+                       self.current_sass_rule.selectors.len() == 0 &&
+                       self.current_sass_rule.children.len() == 0 {
+                           return Some(TopLevelEvent::Comment(SassComment { comment: comment.unwrap() }))
+                    } else {
+                        self.current_sass_rule.children.push(comment.unwrap());
+                        self.pick_something();
+                    }
+                } else {
+                    // is this really what we should be doing here? reachable?
+                    self.state = State::Eof;
+                }
+            } else {
+                println!("i dont know what to do for {:?}", self.state);
+                println!("current sass rule = {:?}", self.current_sass_rule);
+                self.state = State::Eof;
             }
         }
 
