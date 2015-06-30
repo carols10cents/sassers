0dd1b3b8e318046560a695c2bfcf45f01e792597
Carol Nichols <carol.nichols@gmail.com>
2015-06-30 08:40:52 -0400

    Rename st to tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -37,13 +37,13 @@ pub fn compile(inputfile: &str, style: &str) -> Result<String> {
     let mut sass = String::new();
 
     try!(file.read_to_string(&mut sass));
-    let mut st = Tokenizer::new(&sass);
+    let mut tokenizer = Tokenizer::new(&sass);
     match style {
-        "nested"     => Ok(output::nested(&mut st)),
-        "compressed" => Ok(output::compressed(&mut st)),
-        "expanded"   => Ok(output::expanded(&mut st)),
-        "compact"    => Ok(output::compact(&mut st)),
-        "debug"      => Ok(output::debug(&mut st)),
+        "nested"     => Ok(output::nested(&mut tokenizer)),
+        "compressed" => Ok(output::compressed(&mut tokenizer)),
+        "expanded"   => Ok(output::expanded(&mut tokenizer)),
+        "compact"    => Ok(output::compact(&mut tokenizer)),
+        "debug"      => Ok(output::debug(&mut tokenizer)),
         _            => {
             Err(Error {
                 kind: ErrorKind::InvalidStyle,
