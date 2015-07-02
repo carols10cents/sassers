8d98369753f15e0a9cf95b72f04895d7793e8e06
Carol Nichols <carol.nichols@gmail.com>
2015-07-02 08:43:23 -0400

    Read one level of imports into the string before tokenizing

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 extern crate regex;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::File;
 use std::io::Read;
+use regex::Regex;
 
 mod event;
 mod output;
@@ -32,12 +33,41 @@ pub enum ErrorKind {
     InvalidStyle,
 }
 
+fn read_import(inputfile: &PathBuf, buffer: &mut String) {
+    match File::open(&Path::new(inputfile)) {
+        Ok(ref mut file) => {
+            match file.read_to_string(buffer) {
+                Ok(..) => {},
+                Err(e) => panic!("Can't read import file! Filename: {:?} Error: {}", inputfile, e),
+            }
+        },
+        Err(e) => panic!("Can't open import file! Filename: {:?} Error: {}", inputfile, e),
+    }
+}
+
 pub fn compile(inputfile: &str, style: &str) -> Result<String> {
-    let mut file = try!(File::open(&Path::new(inputfile)));
+    let inputpath = PathBuf::from(inputfile);
+    let mut file = try!(File::open(&inputpath));
     let mut sass = String::new();
 
     try!(file.read_to_string(&mut sass));
-    let mut tokenizer = Tokenizer::new(&sass);
+
+    let mut imports_resolved = String::new();
+    for line in sass.split("\n") {
+        let re = Regex::new("@import \"([^\"]*)\";").unwrap();
+
+        match re.captures(line) {
+            Some(caps) => {
+                read_import(&inputpath.with_file_name(caps.at(1).unwrap()), &mut imports_resolved);
+            },
+            None => {
+                imports_resolved.push_str(line);
+                imports_resolved.push_str("\n");
+            },
+        }
+    }
+
+    let mut tokenizer = Tokenizer::new(&imports_resolved);
     match style {
         "nested"     => Ok(output::nested(&mut tokenizer)),
         "compressed" => Ok(output::compressed(&mut tokenizer)),
