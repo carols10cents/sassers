43b0a4f271fb48a37eacb8874d0b33fd6addc067
Carol Nichols <carol.nichols@gmail.com>
2015-08-01 16:30:01 -0700

    Who duplicated these functions they're exactly the same
    Oh wait it was me i'm the only one working on this

--- src/variable_mapper.rs
+++ src/variable_mapper.rs
@@ -17,15 +17,6 @@ impl<I> VariableMapper<I> {
         }
     }
 
-    fn substitute_variables(&self, value: &str) -> String {
-        value.split(' ').map(|value_part|
-            match self.variables.get(value_part) {
-                Some(v) => &v[..],
-                None => value_part,
-            }
-        ).collect::<Vec<_>>().connect(" ")
-    }
-
     fn replace_children_in_scope<'b>(&self, children: Vec<Event<'b>>, starting_variables: &HashMap<String, String>) -> Vec<Event<'b>> {
         let mut local_variables = starting_variables.clone();
 
@@ -67,7 +58,7 @@ impl<'a, I> Iterator for VariableMapper<I>
     fn next(&mut self) -> Option<TopLevelEvent<'a>> {
         match self.tokenizer.next() {
             Some(TopLevelEvent::Variable(SassVariable { name, value })) => {
-                let val = self.substitute_variables(&value);
+                let val = self.substitute_variables_using_locals(&value, &self.variables);
                 self.variables.insert((*name).to_string(), val);
                 self.next()
             },
