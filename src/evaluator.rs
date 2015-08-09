23608b84d76e02d7715577d0d05c5efaa5f31229
Carol Nichols <carol.nichols@gmail.com>
2015-08-08 21:02:49 -0400

    Start test driving value parsing/tokenizing/evaluating
    One test. Just returns one thing right now.

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,10 +1,74 @@
+use sass::variable::SassVariable;
+
+use std::borrow::Cow;
+use std::borrow::Cow::Borrowed;
 use std::collections::HashMap;
 
 pub fn evaluate(original: &str, variables: &HashMap<String, String>) -> String {
-  original.split(' ').map(|original_part|
-      match (*variables).get(original_part) {
-          Some(v) => &v[..],
-          None => original_part,
-      }
-  ).collect::<Vec<_>>().connect(" ")
+    let vt = ValueTokenizer::new(original);
+    original.split(' ').map(|original_part|
+        match (*variables).get(original_part) {
+            Some(v) => &v[..],
+            None => original_part,
+        }
+    ).collect::<Vec<_>>().connect(" ")
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ValuePart<'a> {
+    Variable(SassVariable<'a>),
+    String(Cow<'a, str>),
+}
+
+#[derive(Debug)]
+pub struct ValueTokenizer<'a> {
+    value_str: &'a str,
+    bytes: &'a [u8],
+    offset: usize,
+}
+
+impl<'a> ValueTokenizer<'a> {
+    pub fn new(value_str: &'a str) -> ValueTokenizer<'a> {
+        ValueTokenizer {
+            value_str: &value_str,
+            bytes: &value_str.as_bytes(),
+            offset: 0,
+        }
+    }
+
+    pub fn parse(&mut self) -> Option<ValuePart<'a>> {
+        let start = self.offset;
+        let mut i = self.offset;
+        let limit = self.value_str.len();
+
+        while i < limit {
+            i += 1;
+        }
+        self.offset = i;
+        Some(ValuePart::String(Borrowed(&self.value_str[start..i])))
+    }
+}
+
+impl<'a> Iterator for ValueTokenizer<'a> {
+    type Item = ValuePart<'a>;
+
+    fn next(&mut self) -> Option<ValuePart<'a>> {
+        if self.offset < self.value_str.len() {
+            return self.parse()
+        }
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::borrow::Cow::Borrowed;
+
+    #[test]
+    fn it_returns_string_part() {
+        let mut vt = ValueTokenizer::new("foo");
+        assert_eq!(Some(ValuePart::String(Borrowed(&"foo"))), vt.next());
+        assert_eq!(None, vt.next());
+    }
 }
--- src/sass/variable.rs
+++ src/sass/variable.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct SassVariable<'a> {
     pub name: Cow<'a, str>,
     pub value: Cow<'a, str>,
