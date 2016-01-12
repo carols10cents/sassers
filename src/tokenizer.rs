c0eb737cd6fd5e1216c2837aee72057b1f7e29b7
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-01-11 21:59:16 -0500

    Tokenize single-character sass tokens like curly braces

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,11 +2,11 @@ use token::Token;
 use error::Result;
 
 use std::str::CharIndices;
+use std::iter::Peekable;
 
 pub struct Tokenizer<'a> {
     text: &'a str,
-    chars: CharIndices<'a>,
-    offset: usize,
+    chars: Peekable<CharIndices<'a>>,
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
@@ -25,8 +25,7 @@ impl<'a> Tokenizer<'a> {
     pub fn new(text: &str) -> Tokenizer {
         Tokenizer {
             text: text,
-            chars: text.char_indices(),
-            offset: 0,
+            chars: text.char_indices().peekable(),
         }
     }
 
@@ -38,22 +37,35 @@ impl<'a> Tokenizer<'a> {
             if curr_char.is_whitespace() {
                 continue;
             } else {
-                // First non-whitespace - save its offset to be the Token's offset
-                value.push(curr_char);
-                start = char_offset;
-                while let Some((char_offset, curr_char)) = self.chars.next() {
-                    // Stop when we reach whitespace
-                    if curr_char.is_whitespace() {
-                        break;
-                    } else {
-                        value.push(curr_char);
+                if is_single_char_token(curr_char) {
+                    return Ok(Some(Token {
+                        value: curr_char.to_string(),
+                        offset: Some(char_offset),
+                    }))
+                } else {
+                    // Start of a multi-char token
+                    // First non-whitespace - save its offset to be the Token's offset
+                    value.push(curr_char);
+                    start = char_offset;
+                    while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                        // Stop when we reach a non-word char
+                        if next_char.is_whitespace() || is_single_char_token(next_char) {
+                            break;
+                        } else {
+                            value.push(next_char);
+                            self.chars.next();
+                        }
                     }
+                    return Ok(Some(Token { value: value, offset: Some(start) }))
                 }
             }
-            return Ok(Some(Token { value: value, offset: Some(start) }))
         }
         return Ok(None)
     }
+}
+
+fn is_single_char_token(ch: char) -> bool {
+    "{}:".chars().any(|c| c == ch)
 }
 
 #[cfg(test)]
@@ -85,10 +97,26 @@ mod tests {
     #[test]
     fn it_separates_curly_braces() {
         // Without regard to matching
-        let mut tokenizer = Tokenizer::new("}a{");
-        assert_eq!(tokenizer.next(), Some(Ok(Token::new("}", 0))));
-        assert_eq!(tokenizer.next(), Some(Ok(Token::new("a", 1))));
-        assert_eq!(tokenizer.next(), Some(Ok(Token::new("{", 2))));
+        let mut tokenizer = Tokenizer::new("{}}a{ blue}");
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("{", 0))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("}", 1))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("}", 2))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("a", 3))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("{", 4))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("blue", 6))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("}", 10))));
+        assert_eq!(tokenizer.next(), None);
+    }
+
+    #[test]
+    fn it_separates_colon() {
+        let mut tokenizer = Tokenizer::new(":invalid: property::");
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new(":", 0))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("invalid", 1))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new(":", 8))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new("property", 10))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new(":", 18))));
+        assert_eq!(tokenizer.next(), Some(Ok(Token::new(":", 19))));
         assert_eq!(tokenizer.next(), None);
     }
 }
