61bc2793ca09a03e43a871945104a66e56bd85c6
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-01-13 22:26:16 -0500

    Enough hyphen parsing to continue
    Probably not totally correct yet.

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -32,6 +32,7 @@ impl<'a> Tokenizer<'a> {
     pub fn parse(&mut self) -> Result<Option<Token>> {
         let mut value = String::new();
         let mut start;
+
         while let Some((char_offset, curr_char)) = self.chars.next() {
             // Skip leading whitespace
             if curr_char.is_whitespace() {
@@ -42,6 +43,35 @@ impl<'a> Tokenizer<'a> {
                         value: curr_char.to_string(),
                         offset: Some(char_offset),
                     }))
+                } else if curr_char == '-' {
+                    value.push(curr_char);
+                    start = char_offset;
+
+                    if let Some(&(_, peek_char)) = self.chars.peek() {
+                        if peek_char.is_numeric() {
+                            while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                                // Stop when we reach a non-numeric char
+                                if !next_char.is_numeric() && next_char != '.' {
+                                    break;
+                                } else {
+                                    value.push(next_char);
+                                    self.chars.next();
+                                }
+                            }
+                        } else {
+                            while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                                // Stop when we reach a non-word char
+                                if next_char.is_whitespace() || is_single_char_token(next_char) {
+                                    break;
+                                } else {
+                                    value.push(next_char);
+                                    self.chars.next();
+                                }
+                            }
+                        }
+                    }
+
+                    return Ok(Some(Token { value: value, offset: Some(start) }))
                 } else if curr_char.is_numeric() {
                     // Start of a multi-char numeric token
                     value.push(curr_char);
@@ -59,7 +89,6 @@ impl<'a> Tokenizer<'a> {
                     return Ok(Some(Token { value: value, offset: Some(start) }))
                 } else {
                     // Start of a multi-char non-numeric token
-                    // First non-whitespace - save its offset to be the Token's offset
                     value.push(curr_char);
                     start = char_offset;
                     while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
@@ -168,6 +197,19 @@ mod tests {
         assert_expected_token(tokenizer.next(), "11", 16);
         assert_expected_token(tokenizer.next(), "em", 18);
         assert_expected_token(tokenizer.next(), ";", 20);
+        assert_eq!(tokenizer.next(), None);
+    }
+
+    #[test]
+    fn it_separates_hyphen() {
+        let mut tokenizer = Tokenizer::new("font-weight -webkit -3 - 4-5 a-1");
+        assert_expected_token(tokenizer.next(), "font-weight", 0);
+        assert_expected_token(tokenizer.next(), "-webkit", 12);
+        assert_expected_token(tokenizer.next(), "-3", 20);
+        assert_expected_token(tokenizer.next(), "-", 23);
+        assert_expected_token(tokenizer.next(), "4", 25);
+        assert_expected_token(tokenizer.next(), "-5", 26);
+        assert_expected_token(tokenizer.next(), "a-1", 29);
         assert_eq!(tokenizer.next(), None);
     }
 }
