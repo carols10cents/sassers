486a144610da50c6b6aaf6f401a6bec0a2c70263
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-01-14 22:13:07 -0500

    Add types of tokens

--- src/token.rs
+++ src/token.rs
@@ -1,33 +1,87 @@
 use std::hash;
 use std::fmt;
 
-#[derive(Clone, Debug, Eq)]
-pub struct Token {
-    pub value: String,
+#[derive(Debug, PartialEq)]
+pub struct Lexeme {
+    pub token: Token,
     pub offset: Option<usize>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Token {
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    LeftParen,
+    RightParen,
+    Comma,
+    Colon,
+    Semicolon,
+    LeftCurlyBrace,
+    RightCurlyBrace,
+
+    Ident(String),
+    Variable(String),
+    Literal(String),
+    Comment(String),
+    Number(f32),
+    Color(String),
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
+        match *self {
+            Token::Plus => write!(f, "+"),
+            Token::Minus => write!(f, "-"),
+            Token::Star => write!(f, "*"),
+            Token::Slash => write!(f, "/"),
+            Token::Percent => write!(f, "%"),
+            Token::LeftParen => write!(f, "("),
+            Token::RightParen => write!(f, ")"),
+            Token::Comma => write!(f, ","),
+            Token::Colon => write!(f, ":"),
+            Token::Semicolon => write!(f, ";"),
+            Token::LeftCurlyBrace => write!(f, "{{"),
+            Token::RightCurlyBrace => write!(f, "}}"),
 
-impl PartialEq for Token {
-    fn eq(&self, other: &Token) -> bool { PartialEq::eq(&self.value, &other.value) }
-}
-
-impl hash::Hash for Token {
-    fn hash<H: hash::Hasher>(&self, hasher: &mut H) {
-        (*self.value).hash(hasher)
-    }
-}
-
-impl<'a> From<&'a str> for Token {
-    fn from(s: &'a str) -> Token {
-        Token {
-            value: String::from(s),
-            offset: None,
+            Token::Ident(ref i) => write!(f, "{}", i),
+            Token::Variable(ref i) => write!(f, "{}", i),
+            Token::Literal(ref i) => write!(f, "{}", i),
+            Token::Comment(ref i) => write!(f, "{}", i),
+            Token::Number(i) => write!(f, "{}", i),
+            Token::Color(ref i) => write!(f, "{}", i),
         }
     }
 }
+
+impl Token {
+    pub fn from_char(c: char) -> Option<Token> {
+        let r = match c {
+            '+' => Token::Plus,
+            '-' => Token::Minus,
+            '*' => Token::Star,
+            '/' => Token::Slash,
+            '%' => Token::Percent,
+            '(' => Token::LeftParen,
+            ')' => Token::RightParen,
+            ',' => Token::Comma,
+            ':' => Token::Colon,
+            ';' => Token::Semicolon,
+            '{' => Token::LeftCurlyBrace,
+            '}' => Token::RightCurlyBrace,
+            _   => return None,
+        };
+        Some(r)
+    }
+}
+
+// impl<'a> From<&'a str> for Token {
+//     fn from(s: &'a str) -> Token {
+//         Token {
+//             value: String::from(s),
+//             offset: None,
+//         }
+//     }
+// }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,5 +1,5 @@
-use token::Token;
-use error::Result;
+use token::{Lexeme, Token};
+use error::{Result, SassError, ErrorKind};
 
 use std::str::CharIndices;
 use std::iter::Peekable;
@@ -10,9 +10,9 @@ pub struct Tokenizer<'a> {
 }
 
 impl<'a> Iterator for Tokenizer<'a> {
-    type Item = Result<Token>;
+    type Item = Result<Lexeme>;
 
-    fn next(&mut self) -> Option<Result<Token>> {
+    fn next(&mut self) -> Option<Result<Lexeme>> {
         match self.parse() {
             Ok(Some(t)) => Some(Ok(t)),
             Ok(None) => None,
@@ -29,7 +29,7 @@ impl<'a> Tokenizer<'a> {
         }
     }
 
-    pub fn parse(&mut self) -> Result<Option<Token>> {
+    pub fn parse(&mut self) -> Result<Option<Lexeme>> {
         let mut value = String::new();
         let mut start;
 
@@ -38,69 +38,106 @@ impl<'a> Tokenizer<'a> {
             if curr_char.is_whitespace() {
                 continue;
             } else {
-                if is_single_char_token(curr_char) {
-                    return Ok(Some(Token {
-                        value: curr_char.to_string(),
-                        offset: Some(char_offset),
-                    }))
-                } else if curr_char == '-' {
-                    value.push(curr_char);
-                    start = char_offset;
+                let single_char_token = Token::from_char(curr_char);
+                if value.is_empty() && single_char_token.is_some() {
+                    if curr_char == '-' {
+                        value.push(curr_char);
+                        start = char_offset;
 
-                    if let Some(&(_, peek_char)) = self.chars.peek() {
-                        if peek_char.is_numeric() {
-                            while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
-                                // Stop when we reach a non-numeric char
-                                if !next_char.is_numeric() && next_char != '.' {
-                                    break;
-                                } else {
-                                    value.push(next_char);
-                                    self.chars.next();
+                        if let Some(&(_, peek_char)) = self.chars.peek() {
+                            if peek_char.is_whitespace() {
+                                return Ok(Some(Lexeme { token: Token::Minus, offset: Some(start) }))
+                            } else if peek_char.is_numeric() {
+                                while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                                    // Stop when we reach a non-numeric char
+                                    if !next_char.is_numeric() && next_char != '.' {
+                                        break;
+                                    } else {
+                                        value.push(next_char);
+                                        self.chars.next();
+                                    }
                                 }
-                            }
-                        } else {
-                            while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
-                                // Stop when we reach a non-word char
-                                if next_char.is_whitespace() || is_single_char_token(next_char) {
-                                    break;
-                                } else {
-                                    value.push(next_char);
-                                    self.chars.next();
+
+                                let value = match value.parse() {
+                                    Ok(v) => v,
+                                    Err(e) => return Err(SassError {
+                                        offset: start,
+                                        kind: ErrorKind::TokenizerError,
+                                        message: format!(
+                                            "Tried to parse `{}` into a f32 but failed.",
+                                            value,
+                                        ),
+                                    })
+                                };
+                                return Ok(Some(Lexeme { token: Token::Number(value), offset: Some(start) }))
+
+                            } else {
+                                while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                                    // Stop when we reach a non-ident char (hyphens are special)
+                                    if next_char.is_whitespace() || (
+                                        is_single_char_token(next_char) && next_char != '-'
+                                    ) {
+                                        break;
+                                    } else {
+                                        value.push(next_char);
+                                        self.chars.next();
+                                    }
                                 }
+                                return Ok(Some(Lexeme { token: Token::Ident(value), offset: Some(start) }))
                             }
                         }
+                    } else {
+                        // We already tested that single_char_token was Some.
+                        return Ok(Some(Lexeme {
+                            token: single_char_token.unwrap(),
+                            offset: Some(char_offset),
+                        }))
                     }
-
-                    return Ok(Some(Token { value: value, offset: Some(start) }))
-                } else if curr_char.is_numeric() {
-                    // Start of a multi-char numeric token
-                    value.push(curr_char);
-                    start = char_offset;
-
-                    while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
-                        // Stop when we reach a non-numeric char
-                        if !next_char.is_numeric() && next_char != '.' {
-                            break;
-                        } else {
-                            value.push(next_char);
-                            self.chars.next();
-                        }
-                    }
-                    return Ok(Some(Token { value: value, offset: Some(start) }))
                 } else {
-                    // Start of a multi-char non-numeric token
-                    value.push(curr_char);
-                    start = char_offset;
-                    while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
-                        // Stop when we reach a non-word char
-                        if next_char.is_whitespace() || is_single_char_token(next_char) {
-                            break;
-                        } else {
-                            value.push(next_char);
-                            self.chars.next();
+
+                    if curr_char.is_numeric() {
+                        // Start of a multi-char numeric token
+                        value.push(curr_char);
+                        start = char_offset;
+
+                        while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                            // Stop when we reach a non-numeric char
+                            if !next_char.is_numeric() && next_char != '.' {
+                                break;
+                            } else {
+                                value.push(next_char);
+                                self.chars.next();
+                            }
                         }
+                        let value = match value.parse() {
+                            Ok(v) => v,
+                            Err(e) => return Err(SassError {
+                                offset: start,
+                                kind: ErrorKind::TokenizerError,
+                                message: format!(
+                                    "Tried to parse `{}` into a f32 but failed.",
+                                    value,
+                                ),
+                            })
+                        };
+                        return Ok(Some(Lexeme { token: Token::Number(value), offset: Some(start) }))
+                    } else {
+                        // Start of a multi-char non-numeric token
+                        value.push(curr_char);
+                        start = char_offset;
+                        while let Some(&(next_char_offset, next_char)) = self.chars.peek() {
+                            // Stop when we reach a non-ident char (hyphens are special)
+                            if next_char.is_whitespace() || (
+                                is_single_char_token(next_char) && next_char != '-'
+                            ) {
+                                break;
+                            } else {
+                                value.push(next_char);
+                                self.chars.next();
+                            }
+                        }
+                        return Ok(Some(Lexeme { token: Token::Ident(value), offset: Some(start) }))
                     }
-                    return Ok(Some(Token { value: value, offset: Some(start) }))
                 }
             }
         }
@@ -109,21 +146,21 @@ impl<'a> Tokenizer<'a> {
 }
 
 fn is_single_char_token(ch: char) -> bool {
-    "{}():;/".chars().any(|c| c == ch)
+    Token::from_char(ch).is_some()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use token::Token;
+    use token::{Lexeme, Token};
     use error::Result;
 
-    fn assert_expected_token(
-        actual: Option<Result<Token>>,
-        expected_value: &str,
+    fn assert_expected_lexeme(
+        actual: Option<Result<Lexeme>>,
+        expected_token: Token,
         expected_offset: usize) {
         let actual = actual.unwrap().unwrap();
-        assert_eq!(actual.value, expected_value);
+        assert_eq!(actual.token, expected_token);
         assert_eq!(actual.offset, Some(expected_offset));
     }
 
@@ -143,99 +180,100 @@ mod tests {
     fn it_returns_words() {
         // Without regard to Sass word validity
         let mut tokenizer = Tokenizer::new(" \n  div   aoeu  ");
-        assert_expected_token(tokenizer.next(), "div", 4);
-        assert_expected_token(tokenizer.next(), "aoeu", 10);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("div".into()), 4);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("aoeu".into()), 10);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_curly_braces() {
         // Without regard to matching
-        let mut tokenizer = Tokenizer::new("{}}a{ blue}");
-        assert_expected_token(tokenizer.next(), "{", 0);
-        assert_expected_token(tokenizer.next(), "}", 1);
-        assert_expected_token(tokenizer.next(), "}", 2);
-        assert_expected_token(tokenizer.next(), "a", 3);
-        assert_expected_token(tokenizer.next(), "{", 4);
-        assert_expected_token(tokenizer.next(), "blue", 6);
-        assert_expected_token(tokenizer.next(), "}", 10);
+        let mut tokenizer = Tokenizer::new("{}}a{ blah}");
+        assert_expected_lexeme(tokenizer.next(), Token::LeftCurlyBrace, 0);
+        assert_expected_lexeme(tokenizer.next(), Token::RightCurlyBrace, 1);
+        assert_expected_lexeme(tokenizer.next(), Token::RightCurlyBrace, 2);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("a".into()), 3);
+        assert_expected_lexeme(tokenizer.next(), Token::LeftCurlyBrace, 4);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("blah".into()), 6);
+        assert_expected_lexeme(tokenizer.next(), Token::RightCurlyBrace, 10);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_colon() {
         let mut tokenizer = Tokenizer::new(":invalid: property::");
-        assert_expected_token(tokenizer.next(), ":", 0);
-        assert_expected_token(tokenizer.next(), "invalid", 1);
-        assert_expected_token(tokenizer.next(), ":", 8);
-        assert_expected_token(tokenizer.next(), "property", 10);
-        assert_expected_token(tokenizer.next(), ":", 18);
-        assert_expected_token(tokenizer.next(), ":", 19);
+        // This might be wrong. I have a note somewhere else that colons can start idents.
+        assert_expected_lexeme(tokenizer.next(), Token::Colon, 0);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("invalid".into()), 1);
+        assert_expected_lexeme(tokenizer.next(), Token::Colon, 8);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("property".into()), 10);
+        assert_expected_lexeme(tokenizer.next(), Token::Colon, 18);
+        assert_expected_lexeme(tokenizer.next(), Token::Colon, 19);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_semicolon() {
         let mut tokenizer = Tokenizer::new(";;\na;\nb\n;");
-        assert_expected_token(tokenizer.next(), ";", 0);
-        assert_expected_token(tokenizer.next(), ";", 1);
-        assert_expected_token(tokenizer.next(), "a", 3);
-        assert_expected_token(tokenizer.next(), ";", 4);
-        assert_expected_token(tokenizer.next(), "b", 6);
-        assert_expected_token(tokenizer.next(), ";", 8);
+        assert_expected_lexeme(tokenizer.next(), Token::Semicolon, 0);
+        assert_expected_lexeme(tokenizer.next(), Token::Semicolon, 1);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("a".into()), 3);
+        assert_expected_lexeme(tokenizer.next(), Token::Semicolon, 4);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("b".into()), 6);
+        assert_expected_lexeme(tokenizer.next(), Token::Semicolon, 8);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_numbers() {
         let mut tokenizer = Tokenizer::new("border: 0px 1.5 11em;");
-        assert_expected_token(tokenizer.next(), "border", 0);
-        assert_expected_token(tokenizer.next(), ":", 6);
-        assert_expected_token(tokenizer.next(), "0", 8);
-        assert_expected_token(tokenizer.next(), "px", 9);
-        assert_expected_token(tokenizer.next(), "1.5", 12);
-        assert_expected_token(tokenizer.next(), "11", 16);
-        assert_expected_token(tokenizer.next(), "em", 18);
-        assert_expected_token(tokenizer.next(), ";", 20);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("border".into()), 0);
+        assert_expected_lexeme(tokenizer.next(), Token::Colon, 6);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(0.0), 8);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("px".into()), 9);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(1.5), 12);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(11.0), 16);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("em".into()), 18);
+        assert_expected_lexeme(tokenizer.next(), Token::Semicolon, 20);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_hyphen() {
         let mut tokenizer = Tokenizer::new("font-weight -webkit -3 - 4-5 a-1");
-        assert_expected_token(tokenizer.next(), "font-weight", 0);
-        assert_expected_token(tokenizer.next(), "-webkit", 12);
-        assert_expected_token(tokenizer.next(), "-3", 20);
-        assert_expected_token(tokenizer.next(), "-", 23);
-        assert_expected_token(tokenizer.next(), "4", 25);
-        assert_expected_token(tokenizer.next(), "-5", 26);
-        assert_expected_token(tokenizer.next(), "a-1", 29);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("font-weight".into()), 0);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("-webkit".into()), 12);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(-3.0), 20);
+        assert_expected_lexeme(tokenizer.next(), Token::Minus, 23);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(4.0), 25);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(-5.0), 26);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("a-1".into()), 29);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_parens() {
         let mut tokenizer = Tokenizer::new("() rgb)()(");
-        assert_expected_token(tokenizer.next(), "(", 0);
-        assert_expected_token(tokenizer.next(), ")", 1);
-        assert_expected_token(tokenizer.next(), "rgb", 3);
-        assert_expected_token(tokenizer.next(), ")", 6);
-        assert_expected_token(tokenizer.next(), "(", 7);
-        assert_expected_token(tokenizer.next(), ")", 8);
-        assert_expected_token(tokenizer.next(), "(", 9);
+        assert_expected_lexeme(tokenizer.next(), Token::LeftParen, 0);
+        assert_expected_lexeme(tokenizer.next(), Token::RightParen, 1);
+        assert_expected_lexeme(tokenizer.next(), Token::Ident("rgb".into()), 3);
+        assert_expected_lexeme(tokenizer.next(), Token::RightParen, 6);
+        assert_expected_lexeme(tokenizer.next(), Token::LeftParen, 7);
+        assert_expected_lexeme(tokenizer.next(), Token::RightParen, 8);
+        assert_expected_lexeme(tokenizer.next(), Token::LeftParen, 9);
         assert_eq!(tokenizer.next(), None);
     }
 
     #[test]
     fn it_separates_slash() {
         let mut tokenizer = Tokenizer::new("/ / 3/4 / 8");
-        assert_expected_token(tokenizer.next(), "/", 0);
-        assert_expected_token(tokenizer.next(), "/", 2);
-        assert_expected_token(tokenizer.next(), "3", 4);
-        assert_expected_token(tokenizer.next(), "/", 5);
-        assert_expected_token(tokenizer.next(), "4", 6);
-        assert_expected_token(tokenizer.next(), "/", 8);
-        assert_expected_token(tokenizer.next(), "8", 10);
+        assert_expected_lexeme(tokenizer.next(), Token::Slash, 0);
+        assert_expected_lexeme(tokenizer.next(), Token::Slash, 2);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(3.0), 4);
+        assert_expected_lexeme(tokenizer.next(), Token::Slash, 5);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(4.0), 6);
+        assert_expected_lexeme(tokenizer.next(), Token::Slash, 8);
+        assert_expected_lexeme(tokenizer.next(), Token::Number(8.0), 10);
         assert_eq!(tokenizer.next(), None);
     }
 }
