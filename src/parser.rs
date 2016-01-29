51ce9dc30a643243824e64aeb5dfcc1a408a08b0
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-01-28 22:02:20 -0500

    Be able to parse nested rules again
    Messy messy messy

--- src/parser.rs
+++ src/parser.rs
@@ -25,12 +25,14 @@ impl ASTRoot {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ASTNode {
+    Rule(SassRule),
     Property(Lexeme, Lexeme),
 }
 
 impl ASTNode {
     pub fn stream<W: Write>(&self, output: &mut W, style: SassOutputStyle) -> Result<()> {
         match *self {
+            ASTNode::Rule(ref sr) => try!(sr.stream(output, style)),
             ASTNode::Property(ref name, ref value) => {
                 let ref n = name.token;
                 let ref v = value.token;
@@ -55,18 +57,34 @@ impl<'a> Iterator for Parser<'a> {
 
     fn next(&mut self) -> Option<Result<ASTRoot>> {
         let mut current_sass_rule = SassRule::new();
+        let mut rule_stack = vec![];
         while let Some(Ok(lexeme)) = self.tokenizer.next() {
             if lexeme.token == Token::LeftCurlyBrace {
+                let mut holding_pen = vec![];
                 while let Some(Ok(lexeme)) = self.tokenizer.next() {
                     if lexeme.token == Token::RightCurlyBrace {
-                        return Some(Ok(ASTRoot::Rule(current_sass_rule)))
-                    } else {
-                        self.tokenizer.next(); // colon
+                        if rule_stack.is_empty() {
+                            return Some(Ok(ASTRoot::Rule(current_sass_rule)))
+                        } else {
+                            let tmp_rule = current_sass_rule;
+                            current_sass_rule = rule_stack.pop().unwrap();
+                            current_sass_rule.children.push(ASTNode::Rule(tmp_rule));
+                        }
+                    } else if lexeme.token == Token::LeftCurlyBrace {
+                        rule_stack.push(current_sass_rule);
+                        current_sass_rule = SassRule::new();
+                        current_sass_rule.selectors = holding_pen;
+                        holding_pen = vec![];
+                    } else if lexeme.token == Token::Colon {
+                        // TODO: these unwraps had better work
                         let property_value = self.tokenizer.next().unwrap().unwrap();
-                        self.tokenizer.next(); // semicolon
-
-                        let child = ASTNode::Property(lexeme, property_value);
+                        // TODO: this had better be a semicolon
+                        self.tokenizer.next();
+                        // TODO: holding pen better have exactly one thing in it
+                        let child = ASTNode::Property(holding_pen.pop().unwrap(), property_value);
                         current_sass_rule.children.push(child);
+                    } else {
+                        holding_pen.push(lexeme);
                     }
                 }
                 return Some(Ok(ASTRoot::Rule(current_sass_rule)))
@@ -106,7 +124,29 @@ mod tests {
                 selectors: vec![Lexeme { token: Token::Ident("a".into()), offset: Some(0) }],
                 children: vec![ASTNode::Property(
                     Lexeme { token: Token::Ident("color".into()), offset: Some(4) },
-                    Lexeme { token: Token::Ident("blue".into()), offset: Some(11) }
+                    Lexeme { token: Token::Ident("blue".into()), offset: Some(11) },
+                )],
+            }
+        ))));
+    }
+
+    #[test]
+    fn it_returns_nested_rules() {
+        let mut parser = Parser::new("div { img { color: blue; } }");
+        assert_eq!(parser.next(), Some(Ok(ASTRoot::Rule(
+            SassRule {
+                selectors: vec![Lexeme { token: Token::Ident("div".into()), offset: Some(0) }],
+                children: vec![ASTNode::Rule(
+                    SassRule {
+                        selectors: vec![Lexeme {
+                            token: Token::Ident("img".into()),
+                            offset: Some(6)
+                        }],
+                        children: vec![ASTNode::Property(
+                            Lexeme { token: Token::Ident("color".into()), offset: Some(12) },
+                            Lexeme { token: Token::Ident("blue".into()), offset: Some(19) },
+                        )],
+                    }
                 )],
             }
         ))));
